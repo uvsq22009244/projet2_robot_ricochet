c00688aaed08fa73755bbaf3124233a2a7583fb0
uvsq22009244 <princesse.momo@ens.uvsq.fr>
2021-04-15 21:12:33 +0200

    création du projet ricochet

---
+++ projet_robot_ricochet.py
@@ -0,0 +1,19 @@
+################################################
+
+# Groupe 1 BI TD1
+# Princesse MOMO 
+# Alexandra SCHMIDT
+# Malak LALAMI
+# Nolwenn CORIC
+# Lilian JOANNET
+# Tristan TOLENTINO
+# https://github.com/uvsq22009244/projet_incendie
+
+################################################
+# import des modules
+
+import tkinter as tk
+import random 
+
+################################################
+# constantes
